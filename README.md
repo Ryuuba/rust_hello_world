80df675b2d7d3809a8fe1d23e15886cacc3e9593
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2022-10-21 23:26:45 -0500

    Add hello fn to main.rs

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Description
 
 This repository holds simple, but annotated example intended to understand concepts about the Rust programming language.
+
+To test an example, pass the name of the function to cargo as follows:
+
+```Bash
+cargo run -- example_name example_arg1, example_arg2, example_arg2, ...
+```
+
+where `example_name` is the name of the function to test, `example_arg1` is the first argument of the function, `example_arg2` is the second argument of the function and so on. These arguments depends on the function to be executed.
+
+## hello
+
+This example prints in the terminal a greeting passed via the command line to the function `hello`. Then this function reads from the standard input a name. Finally, the function *greets* the user that inputs their name and exits.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use std::env;
+
+fn hello(greeting: &str) {
+    println!("Who are you?");
+    let mut name: String = String::new();
+    // passing name to read_line as reference
+    io::stdin().read_line(&mut name)
+        .expect("User does not input any name");
+    // println works like Python's print
+    println!("{}, {}", greeting, name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // In this example, collect convert Args-type iterator into a vector of
+    // strings, otherwise, manipulating command-line arguments becomes awkward
+    let argv: Vec<String> = env::args().collect();
+    // as_str returns a &str object from a String
+    match argv[1].as_str() { //match needs a &str object
+        "hello" => hello(argv[2].as_str()),
+        _ => println!("invalid option")
+    }
 }
