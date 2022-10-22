44b8f92d5453dae6ef905aefd1df57787b49a4c3
Adán G. Medrano-Chávez <ryuuba@gmail.com>
2022-10-21 23:30:33 -0500

    Update comments in hello function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 use std::io;
 use std::env;
 
+// Rust function looks like Python's function
 fn hello(greeting: &str) {
     println!("Who are you?");
     let mut name: String = String::new();
     // passing name to read_line as reference
     io::stdin().read_line(&mut name)
         .expect("User does not input any name");
-    // println works like Python's print
+    // println works like Python's print fn
     println!("{}, {}", greeting, name);
 }
 
